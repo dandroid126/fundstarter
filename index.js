b27c6236b29557f1d38e7aec4d00d63c095456e2
Dan Collins <dan.collins126@gmail.com>
2016-03-10 22:40:25 +0000

    index??

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-response.sendfile('fundstarter.html')
+response.sendfile('bitstarter.html') 
 })
 
 app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
