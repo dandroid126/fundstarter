14bcbc650f0060d876984daf3eaa168293a43c44
Dan Collins <dan.collins126@gmail.com>
2016-03-10 22:25:20 +0000

    index 8080

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
 response.sendfile('fundstarter.html') 
 })
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+var port = process.env.PORT || 8080;
+app.listen(app.get(port), function() {
+  console.log("Node app is running at localhost:" + app.get(port))
 })
