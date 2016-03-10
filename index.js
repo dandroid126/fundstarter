383cc58ef9cd9117aadf7120beb43bddf2b06ef3
Dan Collins <dan.collins126@gmail.com>
2016-03-10 22:37:11 +0000

    index??

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-response.sendfile('fundstarter.html') 
+response.sendfile('fundstarter.html')
 })
 
-var port = process.env.PORT || 8080
-app.listen(app.get(port), function() {
-  console.log("Node app is running at localhost:" + app.get(port))
-})
+app.listen(app.get('port'), function() {
